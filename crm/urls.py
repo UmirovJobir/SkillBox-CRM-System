72a39423bb740e7411890ad9cf786e0eb66a4887
Jobir Umirov <jobirumirovmoliya@gmail.com>
2026-04-11 23:16:20 +0500

    home page

--- crm/urls.py
+++ crm/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", user_views.index, name="index"),
     path("products/", include("products.urls")),
     path("ads/", include("advertisements.urls")),
     path("leads/", include("leads.urls")),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from products.models import Product
+from advertisements.models import Advertisement
+from leads.models import Lead
+from customers.models import Customer
 
-# Create your views here.
+
+@login_required
+def index(request):
+    context = {
+        "products_count": Product.objects.count(),
+        "advertisements_count": Advertisement.objects.count(),
+        "leads_count": Lead.objects.count(),
+        "customers_count": Customer.objects.count(),
+    }
+    return render(request, "users/index.html", context)
